00c8f51981a4920af80986900193b117d1d1d796
Priyanka <dhangarpriyanka20@gmail.com>
2021-10-13 05:05:08 +0530

    first commit

---
+++ FirstChar.java
@@ -0,0 +1,16 @@
+/*Assignment-2
+ * Program-2
+ * WAP to Display first character of your program on console
+ * */
+public class FirstChar {
+
+
+	public static void main(String[] args) {
+		//Here Name is priyanka and printing first character of name 
+		char c='p';
+		
+		System.out.println("first charachter of name is>>"+c);
+	}
+	    
+		
+}
---
+++ primitiveDataType.java
@@ -0,0 +1,21 @@
+/*Assignment-2
+ * Program-1
+ * WAP to declare  different primitive datatype..
+ * */
+public class primitiveDataType {
+	public static void main(String[] args) {
+		char c='p';
+		int i=1;
+		byte b=2;
+		short s=3;
+		long l=4;
+		double d=50d;
+		float f=39f;
+		boolean b1=true;
+		//System.out.println("char>>" +c+ "\nint>>"+i+"\nbyte>"+b+"\nshort>>"+s+"\nlong>>"+l+"\ndouble>>"+d+"\nfloat>>"+f+"\nboolean>>"+b1);
+	}
+
+}
+/*OUTPUT
+ * NA(because there only asked about declaration)
+ * */
